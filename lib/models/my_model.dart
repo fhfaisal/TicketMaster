121cdb44b662bf244758644eaec61bdbce78bd24
Faisal <fhfaisallll@gmail.com>
2023-07-06 21:06:42 +0600

    Ticket Master

---
+++ lib/models/my_model.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+class MyModel {
+  String? seat, row, sec, showName, showDetails, showTime;
+  File? selectedImage;
+
+
+  MyModel({
+    this.seat,
+    this.row,
+    this.sec,
+    this.showName,
+    this.showDetails,
+    this.showTime,
+    this.selectedImage,
+  });}
